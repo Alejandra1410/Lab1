ef93e6da985189499ab6c36ac8e69475b0a67bcb
abiga <abigailmolina201@gmail.com>
2023-09-16 12:16:11 -0600

    Clase DebitCard
    Se creo la clase hija que hereda de Card con sus atributos y metodos

---
+++ src/Cards/DebitCard.java
@@ -0,0 +1,28 @@
+package Cards;
+
+/**
+ *
+ * @author abiga
+ */
+public class DebitCard extends Card {
+    //private Account account;
+
+    //public Account getAccount() {
+        //return account;
+    //}
+    
+    public DebitCard(String number, boolean active) {
+        super(number, active);
+    }
+
+   @Override
+   public boolean makePurchase(double amount) {
+      //if (isActive() && amount > 0 && account != null) {
+          //if (account.withdraw(amount)) {
+                //return true;
+           // }
+       // }
+       // return false; 
+   // }
+ }
+    
