1d43b45f4378d3e61df294c4c87d522db6d1d621
abiga <abigailmolina201@gmail.com>
2023-09-16 12:13:55 -0600

    Clase Card
    Se creo la clase abstracta padre con sus atributos y sus metodos

---
+++ src/Cards/Card.java
@@ -0,0 +1,32 @@
+package Cards;
+
+/**
+ *
+ * @author abiga
+ */
+public abstract class Card {//implements Statusontrol
+    private String number;
+    private boolean active;
+
+    public String getNumber() {
+        return number;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public Card(String number, boolean active) {
+        this.number = number;
+        this.active = active;
+    }
+    public void activate() {
+    this.active = true; 
+   }
+
+   public void deactivate() {
+   this.active = false;
+   }
+  public abstract boolean makePurchase(double amount);
+  
+   }
