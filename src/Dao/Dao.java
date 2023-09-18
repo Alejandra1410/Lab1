bf6d732244fa526b119f2e32ce30bef48c5b078a
abiga <abigailmolina201@gmail.com>
2023-09-18 16:22:39 -0600

    Interface Dao
    Se creo la interface Dao

---
+++ src/Dao/Dao.java
@@ -0,0 +1,12 @@
+package Dao;
+
+import java.util.List;
+
+public interface Dao<Class>{
+    public boolean create(Class obj);
+    public Class read(String id);
+    public List <Class> readAll();
+    public boolean update(Class obj);
+    public boolean delete(Class obj);
+    
+}
