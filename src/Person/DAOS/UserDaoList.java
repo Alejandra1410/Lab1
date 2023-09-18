71e9bb8126bf121249684338ec7fff4731dd9def
abiga <abigailmolina201@gmail.com>
2023-09-18 16:24:35 -0600

    Clase UserDaoList
    Se creo la clase userDaoList

---
+++ src/Person/DAOS/UserDaoList.java
@@ -0,0 +1,44 @@
+package Person.DAOS;
+
+import Dao.Dao;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+/*
+ * @author abiga <3
+ */
+public class UserDaoList implements Dao<UserDTO> {
+   private HashMap<String,UserDto> userList;
+
+    public UserDaoList(){
+       userList=new HashMap();
+    }
+   
+    @Override
+    public boolean create(UserDTO user) {
+        if (user==null) return false;
+        userList.put(user.getId(),user);
+        return true;
+    }
+
+    @Override
+    public UserDTO read(String id) {
+        return userList.get(id);
+        }
+
+    @Override
+    public List<UserDTO> readAll() {
+        return new ArrayList<UserDTO>(userList.values()) ;
+       }
+
+    @Override
+    public boolean update(UserDTO user) {
+        return this.create(user);
+    }
+
+    @Override
+    public boolean delete(UserDTO user) {
+        return userList.remove(user.getId())!=null;
+    }
+    
+}
