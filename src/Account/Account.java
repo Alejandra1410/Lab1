428fec4194683500087eb6a7cf6e7358cf0cba3d
EkarCortes <Ekardaco@gmail.com>
2023-09-17 22:02:53 -0600

    Se creo la clase Account
    Se creo la clase Account con su paquete

---
+++ src/Account/Account.java
@@ -0,0 +1,67 @@
+
+package Account;
+
+public abstract class Account {
+    private String number;
+    private double balance;
+    private Customer customer;
+    private boolean active;
+
+    
+    public String getNumber() {
+        return number;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public Account(String number, double balance, Customer customer) {
+        this.number = number;
+        this.balance = balance;
+        this.customer = customer;
+    }
+
+    public Account(String number, Customer customer) {
+        this.number = number;
+        this.customer = customer;
+    }
+    
+    public Account() {
+        active = true; 
+    }
+    public boolean deposit(double amount) {
+    if (amount > 0 && isActive()) {
+        balance += amount; 
+        return true; 
+    } else {
+        return false; 
+    }
+}
+    public boolean withdraw(double amount) {
+        if (amount > 0 && balance >= amount) {
+            balance -= amount;
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+     public boolean transfer(Account destination, double amount) {
+        if (amount > 0 && balance >= amount && destination != null) {
+            balance -= amount;
+            destination.deposit(amount);
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
