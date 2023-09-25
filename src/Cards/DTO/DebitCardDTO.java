5018c33c6c9d6dca0b175a267811bb655feece96
abiga <abigailmolina201@gmail.com>
2023-09-24 19:07:50 -0600

    clase DebitCardDTO
    Se creo la clase DebitCardDTO

--- src/Cards/DTO/DebitCardDTO.java
+++ src/Cards/DTO/DebitCardDTO.java
@@ -1,33 +1,26 @@
-package Cards;
+package Cards.DTO;
 
+import Cards.*;
 import Account.Account;
 
 /**
  *
  * @author abiga
  */
-public class DebitCard extends Card {
+public class DebitCardDTO extends CardDTO {
     private Account account;
 
     public Account getAccount() {
         return account;
     }
-    
-    public DebitCard(String number, Account account) {
-        super(number);
+
+    public DebitCardDTO(Account account, String number, boolean active) {
+        super(number, active);
         this.account = account;
     }
+}
     
 
-   @Override
-   public boolean makePurchase(double amount) {
-      if (isActive() && amount > 0 && account != null) {
-          if (account.withdraw(amount)) {
-                return true;
-            }
-        }
-        return false; 
-    }
- }
+ 
 
     
