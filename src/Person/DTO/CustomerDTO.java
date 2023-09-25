69f7563460717fffbaf0c613abb5ecc3d61fd5d5
abiga <abigailmolina201@gmail.com>
2023-09-24 18:37:50 -0600

    Clase CustomerDTO
    Se creo la clase customer DTO

---
+++ src/Person/DTO/CustomerDTO.java
@@ -0,0 +1,40 @@
+package Person.DTO;
+
+import Person.Person;
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.Period;
+
+/**
+ *
+ * @author abiga
+ */
+public class CustomerDTO extends Person {
+   private Date dateOfBirth;
+   private String phone;
+   private String email;
+   private String address;
+
+    public CustomerDTO(Date dateOfBirth, String phone, String email, String address, String id, String name) {
+        super(id, name);
+        this.dateOfBirth = dateOfBirth;
+        this.phone = phone;
+        this.email = email;
+        this.address = address;
+    }
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    //public int getAge(){
+         //return calculateAge();
+   //}
+}
+  
